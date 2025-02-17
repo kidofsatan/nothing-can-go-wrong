1b722cf81960ad12e2ff2fb5a1cbf1610dcf2499
kidofsatan <sergey_satanov@mail.ru>
2025-02-17 17:41:01 +0300

    First commit

---
+++ Assets/Scripts/Player/NaturalNeeds.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class NaturalNeeds : MonoBehaviour
+{
+    [SerializeField] private float _hunger;
+    [SerializeField] private float _thirst;
+    [SerializeField] private float _stamina;
+}
